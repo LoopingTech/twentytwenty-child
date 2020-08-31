009210eb1638f549f0aef9c0570fe23929265346
Angelica Gardner <angelicahjelmgardner@gmail.com>
2020-08-31 09:41:32 +0200

    Added screenshot.png

--- functions.php
+++ functions.php
@@ -24,3 +24,28 @@ add_action( 'after_setup_theme', 'remove_parent_functionality' );
 function remove_parent_functionality() {
     // Add remove_action() here
 }
+
+/*
+* Enqueue Google fonts for the block editor.
+*/
+add_action( 'enqueue_block_editor_assets', 'my_theme_block)_editor_fonts' );
+function theme_block_editor_fonts() {
+
+    wp_enqueue_style( 'theme-block-editor-fonts', 'URL' );
+    
+}
+
+/**
+ * 
+ */
+add_action( 'after_setup_theme', 'my_theme_setup' );_
+function theme_setup() {
+
+    // This theme styles the visual editor to resemble the theme style.
+    add_editor_style( array( 'inc/editor-style.css' ) );
+    add_theme_support( 'editor-styles' );
+
+    // Enable block styles on the front end
+    add_theme_support( 'wp-block-styles' );
+
+}
